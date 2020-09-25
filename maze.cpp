1ec2db94571220fcc9c3e8da1f6b54de8301c658
Eric Fontaine <eric@ericfontainejazz.com>
2020-09-25 00:26:48 -0400

    Initial Jan25-Feb3,2002 zip archived at eric.bappy.com

---
+++ maze.cpp
@@ -0,0 +1,547 @@
+#include "ptc.h"
+#include <stdlib.h>
+#include <math.h>
+
+#define SW			320
+#define SH			240
+#define kMultiplySW( y ) ( ( (y) << 6 ) + ( (y) << 8 ) )
+
+#define MW			16
+#define MH			16
+
+#define PI			3.1415926535897932384626433832795f
+#define TWOPI		6.283185307179586476925286766559f
+
+#define FOV			( PI / 3.0f )
+
+#define TW			256
+
+#define CW			256
+#define CH			640
+
+#define kSpeedFriction	0.99f
+#define kAngleFriction	0.9f
+
+#define rgb(r,g,b)	( ( (r) << 16 ) | ( (g) << 8 ) | (b) )
+#define r( color )	( ( (color) >> 16 ) & 255 )
+#define g( color )	( ( (color) >> 8 ) & 255 )
+#define b( color )	( (color) & 255 )
+
+float	fPlayerAngle	= 0.0f;
+float	fPlayerDeltaAngle = 0.0f;
+float	fPlayerX		= 0.5f;
+float	fPlayerY		= 0.5f;
+float fPlayerSpeed	= 0.0f;
+float fFishFactor[ SW ];
+
+float fNewX, fNewY;
+int ray,i,row;
+
+int iMipMapTextureOffset[ 9 ];
+int iMipMapLevelRow[ SH/2 ];
+
+bool bHorzWalls[ MH+1 ][ MW ];
+bool bVertWalls[ MH ][ MW+1 ];
+bool bVisited[ MH ][ MW ];
+
+int32 *pixel = NULL;
+int32 *textel= NULL;
+int32 *cloudel=NULL;
+
+int iPosDirs[24][4] = {
+    { 0, 1, 2, 3 }, { 0, 1, 3, 2 }, { 0, 2, 1, 3 }, { 0, 2, 3, 1 }, { 0, 3, 1, 2 }, { 0, 3, 2, 1 },
+    { 1, 0, 2, 3 }, { 1, 0, 3, 2 }, { 1, 2, 0, 3 }, { 1, 2, 3, 0 }, { 1, 3, 0, 2 }, { 1, 3, 2, 0 },
+    { 2, 1, 0, 3 }, { 2, 1, 3, 0 }, { 2, 0, 1, 3 }, { 2, 0, 3, 1 }, { 2, 3, 1, 0 }, { 2, 3, 0, 1 },
+    { 3, 1, 2, 0 }, { 3, 1, 0, 2 }, { 3, 2, 1, 0 }, { 3, 2, 0, 1 }, { 3, 0, 1, 2 }, { 3, 0, 2, 1 }
+};
+
+void vGenerateMaze( int y, int x )
+{
+	bVisited[y][x] = true;
+
+	int i, sequence;
+
+	// top of maze check
+	if( y == 0 )
+	{
+		if( x == 0 )
+		{
+			i=2;
+			sequence = rand() % 2;
+		}
+		else if( x == MW-1 )
+		{
+			i=2;
+			sequence = rand() % 2 + 2;
+		}
+		else
+		{
+			i=1;
+			sequence = rand() % 6;
+		}
+	}
+
+	// bottom of maze check
+	else if( y == MH-1 )
+	{
+		if( x == 0 )
+		{
+			i=2;
+			sequence = rand() % 2 + 18;
+		}
+		else if( x == MW-1 )
+		{
+			i=2;
+			sequence = rand() % 2 + 16;
+		}
+		else
+		{
+			i=1;
+			sequence = rand() % 6 + 18;
+		}
+	}
+
+	// left and right of maze check
+	else if( x == 0 )
+	{
+		i=1;
+		sequence = rand() % 6 + 6;
+	}
+	else if( x == MW-1 )
+	{
+		i=1;
+		sequence = rand() % 6 + 12;
+	}
+
+	// if not an edge
+	else
+	{
+		i=0;
+		sequence = rand() % 24;
+	}
+
+	for( ; i<4; i++ )
+	{	
+		switch( iPosDirs[ sequence ][ i ] )
+		{
+		case 0:
+			if( !bVisited[y-1][x] )
+			{
+				bHorzWalls[y][x] = false;
+				vGenerateMaze( y-1, x );
+			}
+			break;
+		case 1:
+			if( !bVisited[y][x-1] )
+			{
+				bVertWalls[y][x] = false;
+				vGenerateMaze( y, x-1 );
+			}
+			break;
+		case 2:
+			if( !bVisited[y][x+1] )
+			{
+				bVertWalls[y][x+1] = false;
+				vGenerateMaze( y, x+1 );
+			}
+			break;
+		case 3:
+			if( !bVisited[y+1][x] )
+			{
+				bHorzWalls[y+1][x] = false;
+				vGenerateMaze( y+1, x );
+			}
+		}
+	}
+}
+
+void vDrawMaze()
+{
+	int x,y,i;
+	for( y=0; y<MH+1; y++ )
+	{
+		for( x=0; x<MW; x++ )
+		{
+			if( bHorzWalls[y][x] )
+			{
+				for( i=0; i<=2; i++ )
+					pixel[ x*2 + y*2*SW + i ] = 16777215;
+			}
+		}
+	}
+	for( y=0; y<MH; y++ )
+	{
+		for( x=0; x<MW+1; x++ )
+		{
+			if( bVertWalls[y][x] )
+			{
+				for( i=0; i<=2; i++ )
+					pixel[ x*2 + y*2*SW + i*SW ] = 16777215;
+			}
+		}
+	}
+}
+
+#include <fstream.h>
+void load(Surface &surface,char filename[])
+{
+    // open image file
+    ifstream is(filename,ios::in|ios::binary|ios::nocreate);
+
+    // check image file stream
+    if (!is) throw Error("could not load image");
+
+    // skip header
+    is.seekg(54);
+
+    // get surface dimensions
+    const int width  = surface.width();
+    const int height = surface.height();
+    
+    // allocate image pixels
+    char8 *pixels = new char8[width*height*3];
+
+    // read image pixels one line at a time
+    for (int y=height-1; y>=0; y--) is.read((char*)&pixels[width*y*3],width*3);
+
+    // load pixels to surface
+    surface.load(pixels,width,height,width*3,Format(24,0x00FF0000,0x0000FF00,0x000000FF),Palette());
+
+    // free image pixels
+    delete[] pixels;
+}
+
+float x, y, dx, dy, fDistVert, fDistHorz, fDist, fHorzV, fVertV, v, fTanAngle, fLineHeight, du, u, fLineStart, fLineFinish, fLineIntensity;	
+int32 top, bottom;
+float bottomweight, topweight, angle;
+
+void vRaycast()
+{
+	// ray trace
+	angle = fPlayerAngle - FOV / 2.0f;
+	if( angle < 0.0f )
+		angle += TWOPI;
+
+	for( ray=0; ray<SW; ray++ )
+	{
+		fTanAngle = (float) tan( angle );
+
+		// first traverse vertical walls
+		if( angle < .5f*PI || 1.5f*PI < angle )		// if ray going right
+		{
+			dx = 1.0f;
+			dy = fTanAngle;
+			x = float( int( fPlayerX ) + 1 );
+			y = fPlayerY + fTanAngle * ( x - fPlayerX );
+		}
+		else		// if ray going left
+		{
+			dx = -1.0f;
+			dy = -fTanAngle;
+			x = float( int( fPlayerX ) );
+			y = fPlayerY + fTanAngle * ( x - fPlayerX );
+		}
+
+		while( true )
+		{
+			// test if in range
+			if( y < 0.0f || float(MH) <= y )
+				break;
+
+			// test if collide with wall
+			if( bVertWalls[ int(y) ][ int(x) ] )
+				break;
+
+			// move ray
+			x += dx;
+			y += dy;
+		}
+
+		fVertV = y - float( int(y) );
+
+		fDistVert = (float) sqrt( ( x - fPlayerX ) * ( x - fPlayerX ) + ( y - fPlayerY ) * ( y - fPlayerY ) );
+
+		// then traverse horizontal walls
+		if( 0.0f < angle && angle < PI ) // if ray going up
+		{
+			dx = 1.0f / fTanAngle;
+			dy = 1.0f;
+			y = float( int( fPlayerY + 1 ) );
+			x = fPlayerX + 1.0f / fTanAngle * ( y - fPlayerY );
+		}
+		else		// if ray going down
+		{
+			dx = -1.0f / fTanAngle;
+			dy = -1.0f;
+			y = float( int( fPlayerY) );
+			x = fPlayerX + 1.0f / fTanAngle * ( y - fPlayerY );
+		}
+
+
+		while( true )
+		{
+			// test if in range
+			if( x < 0.0f || float(MW) <= x )
+				break;
+
+			// test if collide with wall
+			if( bHorzWalls[ int(y) ][ int(x) ] )
+				break;
+
+			// move ray
+			x += dx;
+			y += dy;
+		}
+
+		fHorzV = x - float( int(x) );
+
+		fDistHorz = (float) sqrt( ( x - fPlayerX ) * ( x - fPlayerX ) + ( y - fPlayerY ) * ( y - fPlayerY ) );
+
+		// determine which is closer
+		if( fDistHorz < fDistVert )
+		{
+			fDist = fDistHorz;
+			v = fHorzV;
+		}
+		else
+		{
+			fDist = fDistVert;
+			v = fVertV;
+		}
+
+		int iMipMapLevel, iMipMapOffset, iMipMapWidth;
+		int iLineFinish, iLineStart, iLineHeight, iHalfLineHeight;
+
+		iHalfLineHeight = int( float(SH/2) / fDist / fFishFactor[ ray ] );
+		iLineHeight = iHalfLineHeight * 2;
+
+		if( iHalfLineHeight < SH/2 )
+			iMipMapLevel = iMipMapLevelRow[ iHalfLineHeight ];
+		else
+			iMipMapLevel = 8;
+
+		iMipMapOffset= iMipMapTextureOffset[ iMipMapLevel ];
+		iMipMapWidth = 1 << iMipMapLevel;
+		du = float(iMipMapWidth) / float(iLineHeight);
+
+		iLineStart = SH/2 - iHalfLineHeight;
+		if( iLineStart < 0 )
+		{
+			u = -du * float(iLineStart);
+			iLineStart = 0;
+		}
+		else
+			u = 0.0f;
+
+		iLineFinish = SH/2 + iHalfLineHeight;
+		if( iLineFinish > SH )
+			iLineFinish = SH;
+
+		// draw clouds
+		int32 *pixeloffset = &pixel[ ray ];
+		int32 *cloudeloffset = &cloudel[ int( angle / TWOPI * float(CH) ) * CW ];
+		int32 *pixelend = pixeloffset + kMultiplySW( iLineStart );
+		float fCloudU = 0.0f;
+		float fCloudDU = float(CW) / float(SH/2);
+		for( ; pixeloffset < pixelend; pixeloffset += SW )
+		{
+			*pixeloffset = cloudeloffset[ int(fCloudU) ];
+			fCloudU += fCloudDU;
+		}
+
+		// actually draw the line
+		int32 *textureoffset = &textel[ int( v * float(iMipMapWidth) ) * TW + iMipMapOffset ];
+		pixelend = &pixel[ ray + kMultiplySW( iLineFinish ) ];
+
+		for( ; pixeloffset < pixelend; pixeloffset += SW )
+		{
+			*pixeloffset = *(textureoffset + int(u) ) ;
+			u += du;
+		}
+
+		float fCosAngle = (float) cos( angle );
+		float fSinAngle = (float) sin( angle );
+
+		float fDeltaScale = -float(SH/2) / fFishFactor[ray];
+		fDist = float(SH/2) / ( fFishFactor[ ray ] * float( iHalfLineHeight ) );
+
+		for( row = iHalfLineHeight; row < SH/2; row++ )
+		{
+			fDist += fDeltaScale / float( row * (row+1) );
+			v = fPlayerX + fCosAngle * fDist;
+			u = fPlayerY + fSinAngle * fDist;
+			iMipMapLevel = iMipMapLevelRow[ row ];
+			iMipMapOffset= iMipMapTextureOffset[ iMipMapLevel ];
+			iMipMapWidth = 1 << (iMipMapLevel);
+			*pixeloffset = textel[ int( ( v - float( int( v ) ) ) * float(iMipMapWidth) ) * TW + int( ( u - float( int( u ) ) ) * float(iMipMapWidth) ) + iMipMapOffset ];
+			pixeloffset += SW;
+		}
+
+		// increment the angle
+		angle += FOV / float(SW);
+		if( angle >= TWOPI )
+			angle -= TWOPI;
+	}
+}
+
+bool bTestIfCanMove()
+{
+	if( fNewX < 0.0f || fNewY < 0.0f )
+		return false;
+
+	if( (int)fNewX > (int)fPlayerX )
+	{
+		if( (int)bVertWalls[ int(fPlayerY) ][ int(fPlayerX) + 1 ] )
+			return false;
+	}
+	if( (int)fNewX < (int)fPlayerX )
+	{
+		if( bVertWalls[ int(fPlayerY) ][ int(fPlayerX) ] )
+			return false;
+	}
+	if( (int)fNewY > (int)fPlayerY )
+	{
+		if(  bHorzWalls[ int(fPlayerY) + 1 ][ int(fPlayerX) ] )
+			return false;
+	}
+	if( (int)fNewY < (int)fPlayerY )
+	{
+		if( bHorzWalls[ int(fPlayerY) ][ int(fPlayerX) ] )
+			return false;
+	}
+	return true;
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
+{
+    try
+    {
+        Console console;
+        Format format(32,0x00FF0000,0x0000FF00,0x000000FF);
+        console.open("Eric Fontaine's Raycasting Maze",SW,SH,format);
+
+        Surface surface(SW,SH,format);
+
+		Surface texture(TW,TW*2,format);
+		load( texture, "wallmipmap.bmp" );
+		textel = (int32 *) texture.lock();
+
+		Surface clouds(CW,CH,format);
+		load( clouds, "clouds.bmp" );
+		cloudel = (int32 *) clouds.lock();
+
+		memset( bHorzWalls, true, sizeof( bHorzWalls ) );
+		memset( bVertWalls, true, sizeof( bVertWalls ) );
+		memset( bVisited, false, sizeof( bVisited ) );
+        
+		srand( GetTickCount() );
+
+		vGenerateMaze( 0, 0 );
+
+		for( ray = 0; ray < SW; ray++ )
+			fFishFactor[ ray ] = (float) cos( ( float(ray) / float(SW) * FOV ) - ( FOV / 2.0f ) );
+
+		for( row = 1; row < SH/2; row ++ )
+		{
+			int iMipTextureRow = 0;
+			int iMipTextureWidth = TW;
+			float dv = float(TW) / float(row*2);
+
+			i = 0;
+			while( dv > 1.0f )
+			{
+				dv /= 2.0f;
+				i++;
+			}
+
+			iMipMapLevelRow[ row ] = 8-i;
+		}
+		iMipMapLevelRow[0] = 0;
+
+		int iMipMapRow = 0;
+		int iMipMapTextureWidth = TW;
+		for( i=0; i<9; i++ )
+		{
+			iMipMapTextureOffset[ 8-i ] = iMipMapRow * TW;
+			iMipMapRow += iMipMapTextureWidth;
+			iMipMapTextureWidth /= 2;
+		}
+
+        while( true )
+        {
+			// get input
+			while( console.key() )
+			{
+				Key key = console.read();
+
+				switch( key.code() )
+				{
+				case Key::LEFT:
+					fPlayerDeltaAngle -= 0.025f;
+					break;
+
+				case Key::RIGHT:
+					fPlayerDeltaAngle += 0.025f;
+					break;
+
+				case Key::UP:
+					fPlayerSpeed += 0.01f;
+					break;
+
+				case Key::DOWN:
+					fPlayerSpeed -= 0.01f;
+					break;
+/*
+				case Key::PAGEDOWN:
+					if( iWallHeight > 0 )
+						iWallHeight--;
+					break;
+
+				case Key::PAGEUP:
+					iWallHeight++;
+					break;
+*/
+				case Key::ESCAPE:
+					texture.unlock();
+					clouds.unlock();
+					return 0;
+				}
+			}
+
+			fPlayerDeltaAngle *= kAngleFriction;
+
+			fPlayerAngle += fPlayerDeltaAngle;
+
+			if( fPlayerAngle < 0.0f )
+				fPlayerAngle += TWOPI;
+
+			else if( fPlayerAngle >= TWOPI )
+				fPlayerAngle -= TWOPI;
+
+			fPlayerSpeed *= kSpeedFriction;
+			fNewY = fPlayerY + (float) sin( fPlayerAngle ) * fPlayerSpeed;
+			fNewX = fPlayerX + (float) cos( fPlayerAngle ) * fPlayerSpeed;
+
+			if( bTestIfCanMove() )
+			{
+				fPlayerY = fNewY;
+				fPlayerX = fNewX;
+			}
+			else
+				fPlayerSpeed = 0.0f;
+
+			pixel = (int32*) surface.lock();
+
+			vRaycast();
+
+			surface.unlock();
+			surface.copy(console);
+			console.update();
+        }
+    }
+    catch (Error &error)
+    {
+        error.report();
+    }
+    return 0;
+}
